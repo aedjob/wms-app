5e1e0e748e78de4aa37d243eddf70cfdd6904bcd
Alfredo <aedjob@gmail.com>
2023-05-20 01:54:32 +1000

    dashboard created

--- wms-app/src/App.jsx
+++ wms-app/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
+import Dashboard from './components/Dashboard/Dashboard'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header />
       <Sidebar />
+      <Dashboard />
     </div>
   )
 }
---
+++ wms-app/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Dashboard.css'
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard-container">
+            <h3>DASHBOARD</h3>
+        </div>
+    )
+}
+
+export default Dashboard
